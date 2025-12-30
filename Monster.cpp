22d733878f55494d446f71cadd06240d68e40744
h-h4 <raphi331110@gmail.com>
2025-12-30 16:21:59 +0900

    Monster.h
    Monster.h, cpp 파일을 작성하였습니다.

--- Monster.cpp
+++ Monster.cpp
@@ -1,1 +1,29 @@
 #include "Monster.h"
+
+	std::string Monster::getName() const {
+		return Name;
+	}
+
+	int Monster::getHealth() const {
+		return Health;
+	}
+
+	int Monster::getattack() const {
+		return Attack;
+	}
+
+	void Monster::takeDamage(int damage) {
+		if (isAlive == false) { return; } // 이미 사망한 몬스터인지 확인
+
+		Health -= damage;
+		if (Health <= 0) {
+			isAlive = false; // 사망시 isAlive 상태를 false로 변경
+			OnDeath();
+		}else{
+		std::cout << getName() << " 의 체력이 " << getHealth() << " 만큼 남았습니다." << std::endl;
+		}
+	}
+
+	void Monster::OnDeath() {
+		std::cout << getName() << " 가 사망했습니다." << std::endl;
+	}
--- Monster.h
+++ Monster.h
@@ -1,5 +1,37 @@
 #pragma once
-class Monster
-{
+#include <iostream>
+#include <string>
+
+class Monster{
+protected:
+	std::string Name;
+	int Health;
+	int Attack;
+	bool isAlive = true;
+
+public:
+  Monster(std::string name, int health, int attack)
+	  : Name(name), Health(health), Attack(attack) {
+  }
+
+  virtual ~Monster() {}
+  
+  std::string getName() const; //몬스터의 이름을 반환합니다.
+
+  int getHealth() const; // 몬스터의 체력을 반환합니다.
+
+  int getattack() const; // 몬스터의 공격력을 반환합니다.
+
+
+  // 몬스터가 데미지를 입었을 때 체력을 감소시키고, 체력이 0 이하가 되면 isAlive 를 false로 변경합니다.
+  void takeDamage(int damage);
+
+  virtual void AttackPlayer() {
+	  std::cout << "이(가) 일반 공격을 합니다!" << std::endl;
+  }
+
+  //몬스터가 죽었을때 호출되는 가상 함수입니다.
+  virtual void OnDeath();
+
 };
 
