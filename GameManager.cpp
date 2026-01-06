55f64e5f3f6596311f22c4f1af6c46ed3cffa7e3
brian521 <brian52125@gmail.com>
2026-01-06 11:09:29 +0900

    bugfix - delete shop 추가

--- GameManager.cpp
+++ GameManager.cpp
@@ -227,6 +227,7 @@ void GameManager::useShop(Character* player)
         case 3:
             cout << "상점을 나갑니다." << endl;
             isContinue = false;
+            delete shop;
             break;
         default:
             break;
